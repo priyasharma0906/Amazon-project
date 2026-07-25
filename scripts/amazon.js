9b002fc1039a14dcc608440b34296a2de2d571e6
Priya Sharma <psharmaps.096@gmail.com>
2026-07-26 02:03:52 +0530

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -76,6 +76,11 @@ document.querySelectorAll('.js-add-to-cart').forEach((button, index) => {
         });
         }
         
-        console.log(cart);
+        let cartQuantity = 0;
+        cart.forEach((item) => {
+          cartQuantity += item.quantity;
+        });
+
+        document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
     });
 });
